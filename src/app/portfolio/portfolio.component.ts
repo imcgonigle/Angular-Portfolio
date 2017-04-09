b7effa84e14ae56f30665460274babd9a8924c73
Ian McGonigle <ian.is.mcgonigle@gmail.com>
2017-04-08 23:07:15 -0600

    create portfolio

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Project } from '../classes/project';
 
 const PROJECTS: Project[] = [
-  {id: 1, title: 'Inventory Application', description: 'A web application that makes it easy to managae inventories from where ever you are.', imageUrls: [], projectUrl: '', isFinished: true },
-  {id: 2, title: 'Route Planner', description: 'A logistics application that helps solve the problem of finding the fastest route.', imageUrls: [], projectUrl: '', isFinished: true },
+  {id: 1, title: 'Inventory Application', description: 'A web application that makes it easy to managae inventories from where ever you are.', imageUrls: [], projectUrl:"https://inventory-application.herokuapp.com/", isFinished: true },
+  {id: 2, title: 'Route Planner', description: 'A logistics application that helps solve the problem of finding the fastest route.', imageUrls: [], projectUrl: 'https://todo-list-2d655.firebaseapp.com', isFinished: true },
   {id: 3, title: 'Zombie Risk', description: 'A Zombie vs Football version of the board game Risk. The project won prizes for best use of MapQuest and best game.', imageUrls: [], projectUrl: '', isFinished: true },
   {id: 4, title: 'Cards Against', description: 'An iOS application version of the popular party game Cards Against Humanity.', imageUrls: [], projectUrl: '', isFinished: true },
   {id: 5, title: 'Denver Eats', description: 'Denver Eats is a landing page and menu website for a fictional resturaunt.', imageUrls: [], projectUrl: '', isFinished: true },
