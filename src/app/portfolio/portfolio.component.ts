bf1ca9b69b3e2cee80a301b33f1d0aa8da62b3c3
Ian McGonigle <ian.is.mcgonigle@gmail.com>
2017-04-08 21:32:28 -0600

    create a projects const

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 
+export class Project {
+  id: number;
+  title: string;
+  imageUrls: string[];
+  isFinished: boolean; 
+}
+
+const PROJECTS: Project[] = [
+  {id: 1, title: 'Inventory Application', imageUrls: [], isFinished: true },
+  {id: 2, title: 'Route Planner', imageUrls: [], isFinished: true },
+  {id: 3, title: 'Zombie Risk', imageUrls: [], isFinished: true },
+  {id: 4, title: 'Cards Against', imageUrls: [], isFinished: true },
+  {id: 5, title: 'Galvanize Eats', imageUrls: [], isFinished: true },
+  {id: 6, title: 'Lesson Box', imageUrls: [], isFinished: false }
+]
+
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
   styleUrls: ['./portfolio.component.scss']
 })
 export class PortfolioComponent implements OnInit {
+  
+  projects = PROJECTS;
 
   constructor() { }
 
   ngOnInit() {
+
   }
 
 }
