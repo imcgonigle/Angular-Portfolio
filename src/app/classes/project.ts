bff48976dc538abb74cac357f520a1d41d3ecd66
Ian McGonigle <ian.is.mcgonigle@gmail.com>
2017-04-09 18:19:41 -0600

    add properties to project class

--- src/app/classes/project.ts
+++ src/app/classes/project.ts
@@ -2,8 +2,12 @@ export class Project {
   id: number;
   title: string;
   description: string;
+  features: string[];
+  technologies: string[];
+  tags: string[];
   imageUrls: string[];
   projectUrl: string;
+  repoUrl: string;
   isFinished: boolean;
-   
+  isFeatured: boolean; 
 }
